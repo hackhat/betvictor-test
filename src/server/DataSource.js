98ea06a0348c05fdcd82ca31923df3c2830be442
hackhat <hackhat.com@gmail.com>
2015-03-23 15:49:57 +0000

    Makes the previous test to pass.

--- src/server/DataSource.js
+++ src/server/DataSource.js
@@ -1,11 +1,17 @@
-var _ = require('lodash');
-var Q = require('q');
+var _       = require('lodash');
+var Q       = require('q');
+var request = require('request');
 
 
 
 
 
 var DataSource = function(){
+    /**
+     * Saves cached data.
+     * @type {Array}
+     * @private
+     */
     this.__data = [];
 }
 
@@ -17,9 +23,27 @@ _.extend(DataSource.prototype, {
 
 
 
-    getData: function(){
+    /**
+     * Returns all the data.
+     * @param  {Object} options
+     * @param  {Boolean} [options.forceRefresh=false] If set to true it will make a refresh before returning
+     *                                                the data.
+     * @return {Q} Returns a Q promise that resolves to an array with several sports' data.
+     */
+    getData: function(options){
+        options = _.extend({
+            forceRefresh: false
+        }, options)
         var deferred = Q.defer();
-        deferred.resolve(this.__data);
+        if(options.forceRefresh){
+            this.refresh().then(function(){
+                deferred.resolve(this.__data.slice(0));
+            }.bind(this), function(err){
+                deferred.reject(err);
+            })
+        }else{
+            deferred.resolve(this.__data.slice(0));
+        }
         return deferred.promise;
     }
 
