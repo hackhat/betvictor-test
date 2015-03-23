828963e1fe7a91ed0bff463fc326a06b5cf2bac8
hackhat <hackhat.com@gmail.com>
2015-03-23 15:29:30 +0000

    Fixes "Data source.should be empty by default" test.

--- test/DataSource.js
+++ test/DataSource.js
@@ -9,12 +9,15 @@ describe('Data source', function(){
 
 
 
-    it('should be empty by default', function(){
+    it('should be empty by default', function(done){
         var DataSource = require('server/DataSource');
         var dataSource = new DataSource();
         dataSource.getData().then(function(data){
             expect(data).to.be.instanceof(Array);
             expect(data).to.be.empty;
+            done();
+        }).catch(function(err){
+            done(err);
         });
     })
 
