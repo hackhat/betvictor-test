0a839bc93be104f8f1fa1e7ca602e874a435f91f
hackhat <hackhat.com@gmail.com>
2015-03-23 16:08:58 +0000

    Adds "Data source.should return some sports if using force refresh" test.

--- test/DataSource.js
+++ test/DataSource.js
@@ -1,5 +1,6 @@
-require    = require('../getWebpackRequire')
-var expect = require("chai").expect;
+require         = require('../getWebpackRequire')
+var expect      = require("chai").expect;
+var appSettings = require('server/settings')();
 
 
 
@@ -32,6 +33,22 @@ describe('Data source', function(){
             return dataSource.getData();
         }).then(function(data){
             expect(data[0]).to.not.equal('changed');
+            done();
+        }).catch(function(err){
+            done(err);
+        });
+    })
+
+
+
+    it('should return some sports if using force refresh', function(done){
+        var DataSource = require('server/DataSource');
+        var dataSource = new DataSource({
+            url: appSettings.dataSourceUrl
+        });
+        dataSource.getData({forceRefresh: true}).then(function(data){
+            expect(data).to.be.instanceof(Array);
+            expect(data).to.have.length.above(0);;
             done();
         }).catch(function(err){
             done(err);
