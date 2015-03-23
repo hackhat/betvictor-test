5f7dd45af5e1ccc762f4bfed0580afed4849e62d
hackhat <hackhat.com@gmail.com>
2015-03-23 15:02:23 +0000

    Adds Data source.should be empty by default test.

---
+++ test/DataSource.js
@@ -0,0 +1,23 @@
+require    = require('../getWebpackRequire')
+var expect = require("chai").expect;
+
+
+
+
+
+describe('Data source', function(){
+
+
+
+    it('should be empty by default', function(){
+        var DataSource = require('server/DataSource');
+        var dataSource = new DataSource();
+        dataSource.getData().then(function(data){
+            expect(data).to.be.instanceof(Array);
+            expect(data).to.be.empty;
+        });
+    })
+
+
+
+})
