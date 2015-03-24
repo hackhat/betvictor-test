957d944eb5e6ce4585aee024d2f3f7ad13ad0ae8
hackhat <hackhat.com@gmail.com>
2015-03-24 09:56:32 +0000

    Refactors test code.

--- test/DataSource.js
+++ test/DataSource.js
@@ -35,97 +35,105 @@ describe('Data source', function(){
 
 
 
-    it('should be empty by default', function(done){
-        dataSource.getData().then(function(data){
-            expect(data).to.be.instanceof(Array);
-            expect(data).to.be.empty;
-            done();
-        }).catch(function(err){
-            done(err);
-        });
-    })
+    describe('.getData()', function(){
 
 
 
-    it('should return a shallow copy of the data', function(done){
-        dataSource.getData().then(function(data){
-            expect(data).to.be.instanceof(Array);
-            data[0] = 'changed';
-            return dataSource.getData();
-        }).then(function(data){
-            expect(data[0]).to.not.equal('changed');
-            done();
-        }).catch(function(err){
-            done(err);
-        });
-    })
+        it('should be empty by default', function(done){
+            dataSource.getData().then(function(data){
+                expect(data).to.be.instanceof(Array);
+                expect(data).to.be.empty;
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
 
 
 
-    it('should return some sports if using force refresh', function(done){
-        __sandbox.stub(request, 'get')
-            .withArgs(appSettings.dataSourceUrl)
-            .yieldsAsync(void 0, {statusCode: 200}, JSON.stringify(liveData));
-        dataSource.getData({forceRefresh: true}).then(function(data){
-            expect(data).to.be.instanceof(Array);
-            expect(data).to.have.length.above(0);
-            done();
-        }).catch(function(err){
-            done(err);
-        });
-    })
+        it('should return a shallow copy of the data', function(done){
+            dataSource.getData().then(function(data){
+                expect(data).to.be.instanceof(Array);
+                data[0] = 'changed';
+                return dataSource.getData();
+            }).then(function(data){
+                expect(data[0]).to.not.equal('changed');
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
 
 
 
-    it('should throw an error if data not available', function(done){
-        var errorName = 'Stub error';
-        __sandbox.stub(request, 'get')
-            .withArgs(appSettings.dataSourceUrl)
-            .yieldsAsync(new Error('Stub error'));
-        dataSource.getData({forceRefresh: true}).then(function(data){
-            done(new Error('Should throw an error.'));
-        }, function(err){
-            expect(err).to.be.ok;
-            expect(err.message).to.be.equal(errorName);
-            done();
-        }).catch(function(err){
-            done(err);
-        });
-    })
+        it('should return some sports if using force refresh', function(done){
+            __sandbox.stub(request, 'get')
+                .withArgs(appSettings.dataSourceUrl)
+                .yieldsAsync(void 0, {statusCode: 200}, JSON.stringify(liveData));
+            dataSource.getData({forceRefresh: true}).then(function(data){
+                expect(data).to.be.instanceof(Array);
+                expect(data).to.have.length.above(0);
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
 
 
 
-    it('should throw "Status code not 200" error if response status code is not 200', function(done){
-        __sandbox.stub(request, 'get')
-            .withArgs(appSettings.dataSourceUrl)
-            .yieldsAsync(void 0, {statusCode: 404}, void 0);
-        dataSource.getData({forceRefresh: true}).then(function(data){
-            done(new Error('Should throw an error.'));
-        }, function(err){
-            expect(err).to.be.ok;
-            expect(err.message).to.be.equal('Status code not 200');
-            done();
-        }).catch(function(err){
-            done(err);
-        });
-    })
+        it('should throw an error if data not available', function(done){
+            var errorName = 'Stub error';
+            __sandbox.stub(request, 'get')
+                .withArgs(appSettings.dataSourceUrl)
+                .yieldsAsync(new Error('Stub error'));
+            dataSource.getData({forceRefresh: true}).then(function(data){
+                done(new Error('Should throw an error.'));
+            }, function(err){
+                expect(err).to.be.ok;
+                expect(err.message).to.be.equal(errorName);
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
 
 
 
-    it('should throw "Invalid JSON" error if response status code is not 200', function(done){
-        var invalidJSONBody = 'sa';
-        __sandbox.stub(request, 'get')
-            .withArgs(appSettings.dataSourceUrl)
-            .yieldsAsync(void 0, {statusCode: 200}, invalidJSONBody);
-        dataSource.getData({forceRefresh: true}).then(function(data){
-            done(new Error('Should throw an error.'));
-        }, function(err){
-            expect(err).to.be.ok;
-            expect(err.message).to.be.equal('Invalid JSON');
-            done();
-        }).catch(function(err){
-            done(err);
-        });
+        it('should throw "Status code not 200" error if response status code is not 200', function(done){
+            __sandbox.stub(request, 'get')
+                .withArgs(appSettings.dataSourceUrl)
+                .yieldsAsync(void 0, {statusCode: 404}, void 0);
+            dataSource.getData({forceRefresh: true}).then(function(data){
+                done(new Error('Should throw an error.'));
+            }, function(err){
+                expect(err).to.be.ok;
+                expect(err.message).to.be.equal('Status code not 200');
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
+
+
+
+        it('should throw "Invalid JSON" error if response status code is not 200', function(done){
+            var invalidJSONBody = 'sa';
+            __sandbox.stub(request, 'get')
+                .withArgs(appSettings.dataSourceUrl)
+                .yieldsAsync(void 0, {statusCode: 200}, invalidJSONBody);
+            dataSource.getData({forceRefresh: true}).then(function(data){
+                done(new Error('Should throw an error.'));
+            }, function(err){
+                expect(err).to.be.ok;
+                expect(err.message).to.be.equal('Invalid JSON');
+                done();
+            }).catch(function(err){
+                done(err);
+            });
+        })
+
+
+
     })
 
 
