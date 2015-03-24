f9e0fd2aa1140548093db6566f79e26db9df2188
hackhat <hackhat.com@gmail.com>
2015-03-24 12:35:38 +0000

    Passes previous test. Creates a server api.

--- src/server/boot.js
+++ src/server/boot.js
@@ -1,1 +1,29 @@
-console.log('boot')
+var express    = require('express');
+var DataSource = require('./DataSource');
+var settings   = require('./settings')();
+
+
+
+
+
+module.exports = function(options){
+    var app = express();
+
+
+
+    var dataSource = new DataSource({
+        url: settings.dataSourceUrl
+    });
+
+
+
+    app.get('/api/sports', function(req, res){
+        dataSource.getSports({forceRefresh: true}).then(function(sports){
+            res.status(200).json(sports);
+        })
+    });
+
+
+
+    return app;
+}
