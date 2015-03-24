10649c637b02db361f1cf1c92ed54cf78f04c2f9
hackhat <hackhat.com@gmail.com>
2015-03-24 13:18:33 +0000

    Boot now returns a promise to be consistent with the rest of the app. The promise resolves with a function to refresh the data externally.

--- src/server/boot.js
+++ src/server/boot.js
@@ -1,13 +1,24 @@
 var express    = require('express');
 var DataSource = require('./DataSource');
 var settings   = require('./settings')();
+var Q          = require('q');
 
 
 
 
-
+/**
+ * @class server.boot
+ * Returns an object with data about the server.
+ * Note: this will not make the server to listen on any port.
+ * @param  {Object}   options
+ * @return {Q} Returns a promise with data about the server and has the following format:
+ *                  data.app          : is the express server instance (not yet listening to any port)
+ *                  data.forceRefresh : a function that can be called to refresh DataSource's data.
+ *                                      Returns a promise.
+ */
 module.exports = function(options, cb){
-    var app = express();
+    var deferred = Q.defer();
+    var app      = express();
 
 
 
@@ -34,12 +45,24 @@ module.exports = function(options, cb){
 
 
 
+    var forceRefresh = function(){
+        return dataSource.refresh();
+    }
 
-    dataSource.refresh().then(function(){
-        cb(void 0, {
-            app: app
+
+
+    // The first time the app startup needs to refresh the dataSource.
+    // In this way when you reach the server is in a proper state and you never
+    // reach a server in a null state.
+    forceRefresh().then(function(){
+        deferred.resolve({
+            app          : app,
+            forceRefresh : function(){
+                return forceRefresh();
+            }
         })
     }).catch(function(err){
-        cb(err);
+        deferred.reject(err);
     })
+    return deferred.promise;
 }
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,6 @@
  * This file is the first file you will run when running the server.
  */
 require = require('../../getWebpackRequire');
-require('server/boot')({}, function(err, data){
+require('server/boot')({}).then(function(data){
     data.app.listen(80);
 });
